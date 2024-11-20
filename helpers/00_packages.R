e1098263a99f94fa508da4ec5421432488b5a8c9
Matthew Coghill <matthewcoghill@live.ca>
2024-11-20 15:27:22 -0800

    Code formatting

--- helpers/00_packages.R
+++ helpers/00_packages.R
@@ -2,47 +2,56 @@
 # before starting!
 
 # CRAN packages
-ls <- c("tidyverse", "lidR", "sf", "sfheaders", "future.apply", "terra", 
+ls <- c(
+  "tidyverse", "lidR", "sf", "sfheaders", "future.apply", "terra",
   "data.table", "units", "rmapshaper", "lwgeom", "RCSF", "lme4", "devtools",
   "RStoolbox", "mlr3verse", "mlr3spatial", "mlr3spatiotempcv", "corrplot",
-  "readxl", "vip", "ranger", "Rsagacmd", "clhs", "raster", "bcmaps", "bcdata")
+  "readxl", "vip", "ranger", "Rsagacmd", "clhs", "raster", "bcmaps", "bcdata"
+)
 
 new_packages <- ls[!(ls %in% installed.packages()[, "Package"])]
-if(length(new_packages)) install.packages(new_packages)
-if(!"lasR" %in% installed.packages()[, "Package"])
+if (length(new_packages)) install.packages(new_packages)
+if (!"lasR" %in% installed.packages()[, "Package"]) {
   install.packages("lasR", repos = "https://r-lidar.r-universe.dev")
+}
 
 # ClimateNAr package
 # First, check if ClimateNAr is already installed and up to date.
-if("ClimateNAr" %in% rownames(installed.packages())) {
-  if(as.Date("2024-10-01") <= packageDate("ClimateNAr")) {
+if ("ClimateNAr" %in% rownames(installed.packages())) {
+  if (as.Date("2024-10-01") <= packageDate("ClimateNAr")) {
     i <- FALSE
-  } else i <- TRUE
-} else i <- TRUE
+  } else {
+    i <- TRUE
+  }
+} else {
+  i <- TRUE
+}
 
 # Download the ClimateNAr package if needed. It is not on CRAN, so you will need
 # to make sure that you can download it either through this link, or create
 # an account and download it there. Link for the package download:
-if(i) {
+if (i) {
   options(timeout = Inf)
   url <- "https://climatena.ca/downloads/ClimateNAr.zip"
   dl_path <- file.path(tempdir(), "ClimateNAr.zip")
   download.file(url, destfile = dl_path, method = "wget")
-  
+
   # Next, unzip the file into your R package installation folder
   unzip(dl_path, exdir = .libPaths()[1])
   # install.packages(dl_path, repos = NULL, type = "source")
 }
 
 # SAGA GIS - Choose version (check at https://sourceforge.net/projects/saga-gis/)
-# A note on versions: There is a bug from version 9.5.1 on that prevents the 
+# A note on versions: There is a bug from version 9.5.1 on that prevents the
 # "Basic Terrain Analysis" tool to proceed. I have notified the SAGA GIS team
 # of this issue, hopefully it will be fixed in future versions.
-if(.Platform$OS.type == "windows") {
+if (.Platform$OS.type == "windows") {
   saga_ver <- "9.6.1"
-  url <- paste0("https://sourceforge.net/projects/saga-gis/files/SAGA%20-%20",
-                strsplit(saga_ver, "\\.")[[1]][1], "/SAGA%20-%20",
-                saga_ver, "/saga-", saga_ver, "_x64.zip/")
+  url <- paste0(
+    "https://sourceforge.net/projects/saga-gis/files/SAGA%20-%20",
+    strsplit(saga_ver, "\\.")[[1]][1], "/SAGA%20-%20",
+    saga_ver, "/saga-", saga_ver, "_x64.zip/"
+  )
   saga_dir <- file.path("C:/SAGA-GIS")
   dir.create(saga_dir, showWarnings = FALSE)
   saga_zip <- file.path(saga_dir, paste0("saga-", saga_ver, "_x64.zip"))
@@ -51,7 +60,7 @@ if(.Platform$OS.type == "windows") {
   unlink(saga_zip)
   saga_tc <- file.path(saga_dir, paste0("saga-", saga_ver, "_x64"), "tools/toolchains")
 } else {
-  warning("The below commands are likely very machine specific, 
+  warning("The below commands are likely very machine specific,
           \ruse these as a general guidance only!")
   system("sudo apt-get install saga")
   saga_tc <- file.path("/usr/share/saga/toolchains")
